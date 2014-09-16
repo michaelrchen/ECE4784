cc6c1f57b83d5e6e94b98328a6681b930d47f1eb
michaelrchen <michaelrchen@gatech.edu>
2014-09-16 13:43:44 -0400

    Update PhaseI.m

--- PhaseI.m
+++ PhaseI.m
@@ -16,25 +16,32 @@ V_rest = -70;
 
 %% steady-state neuron
 % plot resting membrane potential
-V(1:101) = V_rest;
-figure
-plot(t,V)
+V_m(1:101) = V_rest;
+figure(1)
+plot(t,V_m)
 xlabel('Time (ms)')
-ylabel('Voltage (mV)')
+ylabel('Membrane Potential (mV)')
 
-% calculate g_Na/g_K and plot g_Na & g_K
-r = g_Na/g_K;
-gNa(1:101) = g_Na;
-gK(1:101) = g_K;
-figure
-plot(t,gNa)
-hold
-plot(t,gK)
+% calculate resting g_Na/g_K and plot g_Na & g_K
+r(1:101) = g_Na/g_K;
+figure(2)
+plot(t,r)
 xlabel('Time (ms)')
-ylabel('Conductance (mS/cm^2)')
+ylabel('g_{Na}/g_K')
 
 %% step pulse stimulation
+% determine currents
+I_Na = m^3*g_Na*h*(V_m-E_Na);
+I_K = n^4*g_K*(V_m-E_K);
+I_L = g_L*(V_m-E_L);
+I_ion(1:.5:10) = 0-I_K-I_Na-I_L;
+I_ion(11:.5:11.5) = .005-I_K-I_Na-I_L;
+I_ion(12:.5:101) = 0-I_K-I_Na-I_L;
 
 
 %% constant stimulation
-
+% determine currents
+I_Na = m^3*g_Na*h*(V_m-E_Na);
+I_K = n^4*g_K*(V_m-E_K);
+I_L = g_L*(V_m-E_L);
+I_ion(1:100) = .001-I_K-I_Na-I_L;
