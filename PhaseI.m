9c827a0331553e46f87c08d6f245edc36988abb5
michaelrchen <michaelrchen@gatech.edu>
2014-09-18 01:10:38 -0400

    Update PhaseI.m

--- PhaseI.m
+++ PhaseI.m
@@ -6,15 +6,15 @@ clc
 t = 0:100; %ms
 
 % constants
-g_K = 36;
-g_Na = 120;
-g_L = 0.3;
+gbar_K = 36;
+gbar_Na = 120;
+gbar_L = 0.3;
 E_K = -12;
 E_Na = 115;
 E_L = 10.6;
 V_rest = -70;
 
-%% steady-state neuron
+%% steady-state
 % plot resting membrane potential
 V_m(1:101) = V_rest;
 figure(1)
@@ -22,8 +22,25 @@ plot(t,V_m)
 xlabel('Time (ms)')
 ylabel('Membrane Potential (mV)')
 
-% calculate resting g_Na/g_K and plot g_Na & g_K
+% gating variables
+a_m = .1*((25-V_rest)/exp((25-V_rest)/10-1));
+B_m = 4*exp(-V_rest/18);
+a_n = .01*((10-V_rest)/(exp((10-V_rest)/10)-1));
+B_n = .125*exp(-V_rest/80);
+a_h = .07*exp(-V_rest/20);
+B_h = 1/(exp((30-V_rest)/10)+1);
+
+% determine probabilities of subunits being open
+m = a_m/(a_m+B_m);
+n = a_n/(a_n+B_n);
+h = a_h/(a_h+B_h);
+
+% determine g_Na, g_K, & g_Na/g_K ratio
+g_Na = m^3*gbar_Na*h;
+g_K = n^4*gbar_K;
 r(1:101) = g_Na/g_K;
+
+% plot g_Na/g_K ratio
 figure(2)
 plot(t,r)
 xlabel('Time (ms)')
@@ -31,31 +48,34 @@ ylabel('g_{Na}/g_K')
 
 %% step pulse stimulation
 % initialization
-V_m = V_rest;
-t = .5;
+I = .005;
+t_s = .5;
 
 % gating variables
-a_m = .1*((25-V_m)/exp((25-V_m)/10)-1);
+a_m = .1*((25-V_m)/exp((25-V_m)/10-1));
 B_m = 4*exp(-V_m/18);
 a_n = .01*((10-V_m)/(exp((10-V_m)/10)-1));
 B_n = .125*exp(-V_m/80);
 a_h = .07*exp(-V_m/20);
 B_h = 1/(exp((30-V_m)/10)+1);
 
-% state variables
-m = (-a_m/(a_m+B_m))*exp(t*(-a_m-B_m))+(a_m/(a_m+B_m));
-n = (-a_n/(a_n+B_n))*exp(t*(-a_n-B_n))+(a_n/(a_n+B_n));
-h = (-a_h/(a_h+B_h))*exp(t*(-a_h-B_h))+(a_h/(a_h+B_h));
+% calculate probabilities of subunits being open
+m = (-a_m/(a_m+B_m))*exp(t_s*(-a_m-B_m))+(a_m/(a_m+B_m));
+n = (-a_n/(a_n+B_n))*exp(t_s*(-a_n-B_n))+(a_n/(a_n+B_n));
+h = (-a_h/(a_h+B_h))*exp(t_s*(-a_h-B_h))+(a_h/(a_h+B_h));
 
 % determine currents
-I_Na = m^3*g_Na*h*(V_m-E_Na);
-I_K = n^4*g_K*(V_m-E_K);
+I_Na = m^3*gbar_Na*h*(V_m-E_Na);
+I_K = n^4*gbar_K*(V_m-E_K);
 I_L = g_L*(V_m-E_L);
 I_ion = I-I_K-I_Na-I_L;
 
+% determine membrane potential
+V_m = .5*I_ion/C_m;
+
 %% constant stimulation
 % determine currents
-I_Na = m^3*g_Na*h*(V_m-E_Na);
-I_K = n^4*g_K*(V_m-E_K);
-I_L = g_L*(V_m-E_L);
+I_Na = m^3*gbar_Na*h*(V_m-E_Na);
+I_K = n^4*gbar_K*(V_m-E_K);
+I_L = gbar_L*(V_m-E_L);
 I_ion(1:100) = .001-I_K-I_Na-I_L;
