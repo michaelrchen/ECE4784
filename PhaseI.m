be43fbf2d93bc5d8e0c2f3a0333302caf47ee3a5
michaelrchen <michaelrchen@gatech.edu>
2014-09-18 17:25:09 -0400

    Update PhaseI.m

--- PhaseI.m
+++ PhaseI.m
@@ -13,6 +13,7 @@ E_K = -12;
 E_Na = 115;
 E_L = 10.6;
 V_rest = -70;
+C_m = .001;
 
 %% steady-state
 % plot resting membrane potential
@@ -22,7 +23,7 @@ plot(t,V_m)
 xlabel('Time (ms)')
 ylabel('Membrane Potential (mV)')
 
-% gating variables
+% calculate gating variables at rest
 a_m = .1*((25-V_rest)/exp((25-V_rest)/10-1));
 B_m = 4*exp(-V_rest/18);
 a_n = .01*((10-V_rest)/(exp((10-V_rest)/10)-1));
@@ -30,14 +31,13 @@ B_n = .125*exp(-V_rest/80);
 a_h = .07*exp(-V_rest/20);
 B_h = 1/(exp((30-V_rest)/10)+1);
 
-% determine probabilities of subunits being open
-m = a_m/(a_m+B_m);
-n = a_n/(a_n+B_n);
-h = a_h/(a_h+B_h);
+m_0 = a_m/(a_m+B_m);
+n_0 = a_n/(a_n+B_n);
+h_0 = a_h/(a_h+B_h);
 
-% determine g_Na, g_K, & g_Na/g_K ratio
-g_Na = m^3*gbar_Na*h;
-g_K = n^4*gbar_K;
+% calculate g_Na, g_K, & g_Na/g_K ratio
+g_Na = m_0^3*gbar_Na*h_0;
+g_K = n_0^4*gbar_K;
 r(1:101) = g_Na/g_K;
 
 % plot g_Na/g_K ratio
@@ -51,13 +51,19 @@ ylabel('g_{Na}/g_K')
 I = .005;
 t_s = .5;
 
+% calculate resting currents
+I_K = g_K*(V_rest-E_K);
+I_Na = g_Na*(V_rest-E_Na);
+I_L = -(I_K+I_Na);
+I_ion = 0-I_K-I_Na-I_L;
+
 % gating variables
-a_m = .1*((25-V_m)/exp((25-V_m)/10-1));
-B_m = 4*exp(-V_m/18);
-a_n = .01*((10-V_m)/(exp((10-V_m)/10)-1));
-B_n = .125*exp(-V_m/80);
-a_h = .07*exp(-V_m/20);
-B_h = 1/(exp((30-V_m)/10)+1);
+a_m = .1*((25-V_rest)/exp((25-V_rest)/10-1));
+B_m = 4*exp(-V_rest/18);
+a_n = .01*((10-V_rest)/(exp((10-V_rest)/10)-1));
+B_n = .125*exp(-V_rest/80);
+a_h = .07*exp(-V_rest/20);
+B_h = 1/(exp((30-V_rest)/10)+1);
 
 % calculate probabilities of subunits being open
 m = (-a_m/(a_m+B_m))*exp(t_s*(-a_m-B_m))+(a_m/(a_m+B_m));
